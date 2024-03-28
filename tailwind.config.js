f761b225cab910120171dbe4703afbca0a21b388
FaycalZM <zemmourisissy@gmail.com>
2024-03-28 04:31:46 +0100

    Add Tailwind configuration (MAPI design system)

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-dark': '#141943',
+        'primary-normal': '#8F00FF',
+        'primary-gray': '#BDBDBD',
+        'primary-darker': '#0E1135',
+        'primary-linear': '#141843',
+        'secondary-blue': '#2F80ED',
+        'secondary-gray': '#E2E2E2',
+        'dark-text': '#00101E',
+        'neutral': '#858C94',
+        'neutral-2': '#DDDDDD',
+        'mapi-primary': '#CB3CFF',
+        'mapi-secondary-1': '#0B1739',
+        'mapi-secondary-2': '#8951FF',
+        'mapi-secondary-3': '#21C3FC',
+        'mapi-secondary-4': '#0E43FB',
+        'mapi-secondary-5': '#FDB52A',
+        'mapi-neutral-1': '#FDB52A',
+        'mapi-neutral-2': '#0A1330',
+        'mapi-neutral-3': '#0B1739',
+        'mapi-neutral-4': '#7E89AC',
+        'mapi-neutral-5': '#AEB9E1',
+        'mapi-neutral-6': '#D1DBF9',
+        'mapi-neutral-7': '#D9E1FA',
+        'mapi-neutral-8': '#FFFFFF',
+        'corner-1': {
+          300: '#7E89AC',
+        },
+        'c-1': '#634CD7',
+        'lineartp': '#69EACB',
+      },
+      fontFamily: {
+        'plus-jakarta-sans': ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      screens: {
+        'desktop': '1281px',
+        'laptop': '1025px',
+        'tablet': '769px',
+      }
+    },
+  },
+  plugins: [],
+}
+
